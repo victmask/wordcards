f05d07f51d967d1ac2b7005ac54de577f2d08cdf
victmask <victmask@gmail.com>
2012-05-28 02:41:18 -0700

    added migrations

--- db/migrate/20120514221131_create_cards.rb
+++ db/migrate/20120514221131_create_cards.rb
@@ -6,11 +6,7 @@ class CreateCards < ActiveRecord::Migration
       t.text :definition
       t.text :example
       t.integer :user_id
-      t.string :uuid, :null => false
-
       t.timestamps
     end
-
-    add_index :cards, :uuid
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120520232730) do
+ActiveRecord::Schema.define(:version => 20120528093852) do
 
   create_table "cards", :force => true do |t|
     t.string   "word"
@@ -19,10 +19,10 @@ ActiveRecord::Schema.define(:version => 20120520232730) do
     t.text     "definition"
     t.text     "example"
     t.integer  "user_id"
-    t.string   "uuid",        :null => false
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "image_src"
+    t.string   "uuid"
   end
 
   add_index "cards", ["uuid"], :name => "index_cards_on_uuid"
