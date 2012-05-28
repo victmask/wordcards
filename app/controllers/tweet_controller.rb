c394f063306b55e92f2d7275ad387597d28db52e
victmask <victmask@gmail.com>
2012-05-28 02:55:02 -0700

    updated tweet url

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -6,7 +6,7 @@ class TweetController < ApplicationController
     Card.all.each do |card|
       #Twitter.update("#{card.word} - view card at http://localhost:3000/cards/#{card.id}")
       begin
-        message = "Learn '#{card.word}' - '#{card.translation[0..40]}'. Go to http://#{request.host_with_port + cards_path(card.uuid)}"
+        message = "Learn '#{card.word}' - '#{card.translation[0..35]}'. Go to #{card_url(card.uuid)}"
         puts message
         Twitter.d(twitter_account, message)
       rescue Exception => e
@@ -27,5 +27,8 @@ class TweetController < ApplicationController
 
   end
 
+  def card_url(card_uuid)
+    "http://#{request.host_with_port}/cards/#{card_uuid}"
+  end
 
 end
