5f98991160e70c3f32359baaf10ce418e393f944
victmask <victmask@gmail.com>
2012-06-13 11:46:59 -0700

    twitter login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+
+  def new
+    #twitter_params = {
+    #        "oauth_version" => "1.0",
+    #        "oauth_nonce" => Time.new.to_i,
+    #        "oauth_timestamp" => Time.new.to_s,
+    #        "oauth_consumer_key" => ENV['TWITTER_CONSUMER_KEY'],
+    #        "oauth_signature_method" => "HMAC-SHA1",
+    #        "oauth_signature" => ENV['TWITTER_CONSUMER_SECRET'],
+    #        "oauth_callback" => CGI.escapeHTML("http://localhost:3000/")
+    #     }
+
+  end
+
+  def create
+    #if params[:twitter_anywhere_identity]
+  end
+end
